348b8ec20c41a51441db6f1497e0a9b113c041de
Eivind Vold Aunebakk <eivind@aunebakk.no>
2018-09-22 01:30:57 +0200

    Fmt and clippy

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -5,20 +5,19 @@ use std::env;
 pub mod models;
 pub mod schema;
 
-use self::models::{User};
+use self::models::User;
 
 pub fn establish_connection() -> MysqlConnection {
-
     dotenv().ok();
 
-    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-
+    let database_url =
+        env::var("DATABASE_URL").unwrap_or_else(|_| panic!("DATABASE_URL must be set"));
     MysqlConnection::establish(&database_url)
-        .expect(&format!("Error connecting to {}", database_url))
+        .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
 }
 
 pub fn insert_user(user_id: i32, username: &str) -> User {
-    use self::schema::users::dsl::{users, id};
+    use self::schema::users::dsl::{id, users};
 
     let connection = establish_connection();
 
@@ -38,43 +37,43 @@ pub fn insert_user(user_id: i32, username: &str) -> User {
     users.order(id.desc()).first(&connection).unwrap()
 }
 
-
 pub fn get_user(user_id: i32) -> User {
     use self::schema::users::dsl::*;
 
     let connection = establish_connection();
 
     println!("Getting user: {}", user_id);
-    users.filter(id.eq(user_id))
+    users
+        .filter(id.eq(user_id))
         .first(&connection)
         .expect("Error getting user")
 }
 
-pub fn delete_user(user_id: i32) -> usize  {
+pub fn delete_user(user_id: i32) -> usize {
     use self::schema::users::dsl::*;
 
     let connection = establish_connection();
 
-     let num_deleted = diesel::delete(users)
+    let num_deleted = diesel::delete(users)
         .filter(id.eq(user_id))
         .execute(&connection)
         .expect("Error deleting user");
-    
-     println!("Deleted {} rows", num_deleted);
+
+    println!("Deleted {} rows", num_deleted);
 
     num_deleted
 }
 
-pub fn delete_all_users() -> usize  {
+pub fn delete_all_users() -> usize {
     use self::schema::users::dsl::*;
 
     let connection = establish_connection();
 
-     let num_deleted = diesel::delete(users)
+    let num_deleted = diesel::delete(users)
         .execute(&connection)
         .expect("Error deleting users");
-    
-     println!("Deleted {} rows", num_deleted);
+
+    println!("Deleted {} rows", num_deleted);
 
     num_deleted
 }
