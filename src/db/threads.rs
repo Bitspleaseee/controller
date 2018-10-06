3befe6cf742dbe91b8448bcff43d327de01b714d
Ole Martin Ruud <barskern@outlook.com>
2018-10-06 23:40:30 +0200

    Implement get and insert for threads (database)

--- src/db/threads.rs
+++ src/db/threads.rs
@@ -8,6 +8,19 @@ use crate::{IntErrorKind, IntResult};
 use datatypes::valid::fields::*;
 use datatypes::valid::ids::*;
 
+const MAX_THREAD_LIMIT: i64 = 30;
+
+/// Simple macro to format a hidden bool
+macro_rules! fmt_hidden {
+    ($hidden:expr) => {
+        if $hidden {
+            "including hidden"
+        } else {
+            "excluding hidden"
+        }
+    };
+}
+
 /// Inserts a new thread into the thread table
 pub fn insert_thread(
     connection: &DbConn,
@@ -16,52 +29,106 @@ pub fn insert_thread(
 ) -> IntResult<Thread> {
     use super::schema::threads::dsl;
 
-    trace!("Inserting thread: {}", title);
-    Err(IntErrorKind::ServerError)?
+    trace!("Inserting thread");
 
-    //diesel::insert_into(dsl::threads)
-    //    .values((
-    //        dsl::title.eq(title.as_ref()),
-    //        dsl::description.eq(description.as_ref()),
-    //    )).execute(connection)
-    //    .context(IntErrorKind::QueryError)
-    //    .and_then(|_| {
-    //        threads
-    //            .order(dsl::id.desc())
-    //            .first(connection)
-    //            .context(IntErrorKind::ContentNotFound)
-    //    }).map_err(|e| e.into())
+    diesel::insert_into(dsl::threads)
+        .values((
+            dsl::title.eq(title.as_ref()),
+            dsl::description.eq(description.as_ref()),
+        )).execute(connection)
+        .context(IntErrorKind::QueryError)
+        .and_then(|_| {
+            dsl::threads
+                .order(dsl::id.desc())
+                .first(connection)
+                .context(IntErrorKind::ContentNotFound)
+        }).map(|t: Thread| {
+            trace!("Inserted thread got id {}", t.id);
+            t
+        }).map_err(|e| {
+            error!("Unable to insert new thread: {}", e);
+            e.into()
+        })
 }
 
 /// Gets an exisiting thread from the thread table
 pub fn get_thread(connection: &DbConn, id: &ThreadId, include_hidden: bool) -> IntResult<Thread> {
     use super::schema::threads::dsl;
 
-    trace!("Getting thread ({:?})", id);
-    Err(IntErrorKind::ServerError)?
+    trace!(
+        "Getting thread with id {:?} ({})",
+        id,
+        fmt_hidden!(include_hidden)
+    );
 
-    //dsl::threads
-    //    .filter(dsl::id.eq(*(*id)))
-    //    .first::<Thread>(connection)
-    //    .optional()
-    //    .context(IntErrorKind::QueryError)?
-    //    .ok_or(IntErrorKind::ContentNotFound)
-    //    .map_err(|e| e.into())
+    if include_hidden {
+        dsl::threads
+            .filter(dsl::id.eq(*(*id)))
+            .first::<Thread>(connection)
+            .optional()
+    } else {
+        dsl::threads
+            .filter(dsl::id.eq(*(*id)))
+            .filter(dsl::hidden.eq(false))
+            .first::<Thread>(connection)
+            .optional()
+    }.context(IntErrorKind::QueryError)?
+    .ok_or(IntErrorKind::ContentNotFound)
+    .map_err(|e| {
+        error!("Unable to get thread with id {}: {}", id, e);
+        e.into()
+    })
 }
 
 /// Gets all the threads from the thread table
 pub fn get_all_threads(connection: &DbConn, include_hidden: bool) -> IntResult<Vec<Thread>> {
     use super::schema::threads::dsl;
 
-    trace!("Getting all threads, include hidden: {}", include_hidden);
-    Err(IntErrorKind::ServerError)?
+    trace!("Getting all threads ({})", fmt_hidden!(include_hidden));
 
-    //if include_hidden {
-    //    threads.get_results(connection)
-    //} else {
-    //    threads.filter(hidden.eq(false)).get_results(connection)
-    //}.context(IntErrorKind::QueryError)
-    //.map_err(|e| e.into())
+    if include_hidden {
+        dsl::threads.limit(MAX_THREAD_LIMIT).get_results(connection)
+    } else {
+        dsl::threads
+            .limit(MAX_THREAD_LIMIT)
+            .filter(dsl::hidden.eq(false))
+            .get_results(connection)
+    }.context(IntErrorKind::QueryError)
+    .map_err(|e| {
+        error!("Unable to get all threads: {}", e);
+        e.into()
+    })
+}
+
+/// Gets all the comments in a thread from the comment table
+pub fn get_threads_in_category(
+    connection: &DbConn,
+    category_id: &CategoryId,
+    include_hidden: bool,
+) -> IntResult<Vec<Thread>> {
+    use super::schema::threads::dsl;
+
+    trace!("Getting threads in category with id {}", category_id);
+
+    if include_hidden {
+        dsl::threads
+            .limit(MAX_THREAD_LIMIT)
+            .filter(dsl::category_id.eq(*(*category_id)))
+            .get_results(connection)
+    } else {
+        dsl::threads
+            .limit(MAX_THREAD_LIMIT)
+            .filter(dsl::category_id.eq(*(*category_id)))
+            .filter(dsl::hidden.eq(false))
+            .get_results(connection)
+    }.context(IntErrorKind::QueryError)
+    .map_err(|e| {
+        error!(
+            "Unable to get threads in category with id {}: {}",
+            category_id, e
+        );
+        e.into()
+    })
 }
 
 /// Clears the thread table
@@ -73,7 +140,10 @@ pub fn delete_all_threads(connection: &DbConn) -> IntResult<usize> {
     diesel::delete(dsl::threads)
         .execute(connection)
         .context(IntErrorKind::QueryError)
-        .map_err(|e| e.into())
+        .map_err(|e| {
+            error!("Unable to delete all threads: {}", e);
+            e.into()
+        })
 }
 
 /// Updates an existing thread in the thread table
@@ -98,8 +168,7 @@ pub fn update_thread(
     if num_updated == 0 {
         Err(IntErrorKind::ContentNotFound)?
     } else {
-        //get_thread(&connection, )
-        Err(IntErrorKind::ContentNotFound)?
+        get_thread(connection, id, false)
     }
 }
 
