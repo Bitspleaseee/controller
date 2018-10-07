7eb8d081839a10869e278818581577256616500d
Ole Martin Ruud <barskern@outlook.com>
2018-10-07 21:01:15 +0200

    Update inspector with more commands

--- src/bin/inspector.rs
+++ src/bin/inspector.rs
@@ -5,11 +5,12 @@
 
 #[macro_use]
 extern crate tarpc;
+extern crate datatypes;
 extern crate futures;
 extern crate rustyline;
 extern crate serde_derive;
-
-extern crate datatypes;
+#[macro_use]
+extern crate failure;
 
 use rustyline::error::ReadlineError;
 use rustyline::Editor;
@@ -24,6 +25,200 @@ use tarpc::util::FirstSocketAddr;
 use datatypes::content::requests::*;
 use datatypes::content::responses::*;
 use datatypes::valid::fields::*;
+use datatypes::valid::ids::*;
+
+use failure::Fallible;
+macro_rules! enum_str {
+    {
+        $( #[ $( $attr:meta ),* ] )*
+        pub enum $name:ident {
+            $( $sub:ident => $sub_str:expr ),*
+        }
+    } => {
+        $( #[ $( $attr ),* ] )*
+        pub enum $name {
+            $( $sub ),*
+        }
+        impl TryFrom<&str> for $name {
+            type Error = failure::Error;
+            fn try_from(s: &str) -> Result<Self, Self::Error> {
+                match s {
+                    $( $sub_str => Ok($name::$sub), )*
+                    s => Err(format_err!("Invalid {}: '{}'", stringify!($name), s))
+                }
+            }
+        }
+        impl std::fmt::Display for $name {
+            fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+                match self {
+                    $( $name::$sub => write!(f, "{}", $sub_str), )*
+                }
+            }
+        }
+    }
+}
+
+enum_str! {
+    #[derive(Copy, Clone)]
+    pub enum Cmd {
+        Get => "get",
+        GetAll => "get-all",
+        Insert => "insert",
+        Hide => "hide",
+        Edit => "edit",
+        Delete => "delete"
+    }
+}
+
+enum_str! {
+    #[derive(Copy, Clone)]
+    pub enum Mode {
+        Main => "main",
+        Users => "users",
+        Categories => "categories",
+        Threads => "threads"
+    }
+}
+
+fn cmd_handler<'a>(state: &State, s: &'a str) -> Fallible<()> {
+    let mut args = s.split(char::is_whitespace);
+    let cmd = args
+        .next()
+        .ok_or(format_err!("Missing argument <cmd>"))
+        .and_then(|w| w.try_into())?;
+    match (state.mode, cmd) {
+        (Mode::Users, Cmd::Get) => run_get_user(args),
+        (Mode::Users, Cmd::Insert) => run_insert_user(args),
+        (Mode::Users, Cmd::Edit) => run_edit_user(args),
+        (Mode::Categories, Cmd::Get) => run_get_category(args),
+        (Mode::Categories, Cmd::GetAll) => run_get_all_categories(args),
+        (Mode::Categories, Cmd::Insert) => run_insert_category(args),
+        (Mode::Categories, Cmd::Edit) => run_edit_category(args),
+        (Mode::Categories, Cmd::Hide) => run_hide_category(args),
+        (m, c) => Err(format_err!(
+            "Unimplemented command '{}' for mode '{}'",
+            c,
+            m
+        )),
+    }
+}
+
+macro_rules! get_next_field {
+    ($args:ident, $name:ident) => {
+        $args
+            .next()
+            .ok_or(format_err!("Missing argument <{}>", stringify!($name)))
+            .and_then(|s| {
+                s.to_owned()
+                    .try_into()
+                    .map_err(|_| format_err!("Invalid <{}>", stringify!($name)))
+            })
+    };
+}
+
+macro_rules! get_next_id {
+    ($args:ident, $inner:ty => $name:ident) => {
+        $args
+            .next()
+            .ok_or(format_err!("Missing argument <{}>", stringify!($name)))
+            .and_then(|s| {
+                s.parse::<$inner>()
+                    .map(|n| n.into())
+                    .map_err(|_| format_err!("Invalid <{}>", stringify!($name)))
+            })
+    };
+}
+
+// User
+
+fn run_get_user<'a>(mut args: impl Iterator<Item = &'a str>) -> Fallible<()> {
+    let id = get_next_id!(args, u32 => user_id)?;
+    let payload = GetUserPayload { id };
+
+    run_client_action(|client| client.get_user(payload));
+    Ok(())
+}
+
+fn run_insert_user<'a>(mut args: impl Iterator<Item = &'a str>) -> Fallible<()> {
+    let id = get_next_id!(args, u32 => user_id)?;
+    let username = get_next_field!(args, username)?;
+    let payload = AddUserPayload { id, username };
+
+    run_client_action(|client| client.add_user(payload));
+    Ok(())
+}
+
+fn run_edit_user<'a>(mut args: impl Iterator<Item = &'a str>) -> Fallible<()> {
+    let id = get_next_id!(args, u32 => user_id)?;
+    let description = get_next_field!(args, description).ok();
+    let avatar = get_next_field!(args, avatar).ok();
+
+    let payload = EditUserPayload {
+        id,
+        description,
+        avatar,
+    };
+
+    run_client_action(|client| client.edit_user(payload));
+    Ok(())
+}
+
+// Category
+
+fn run_get_category<'a>(mut args: impl Iterator<Item = &'a str>) -> Fallible<()> {
+    let id = get_next_id!(args, u32 => user_id)?;
+
+    let payload = GetCategoryPayload {
+        id,
+        include_hidden: true,
+    };
+
+    run_client_action(|client| client.get_category(payload));
+    Ok(())
+}
+
+fn run_get_all_categories<'a>(mut _args: impl Iterator<Item = &'a str>) -> Fallible<()> {
+    let payload = GetHiddenPayload {
+        include_hidden: true,
+    };
+
+    run_client_action(|client| client.get_all_categories(payload));
+    Ok(())
+}
+
+fn run_insert_category<'a>(mut args: impl Iterator<Item = &'a str>) -> Fallible<()> {
+    let title = get_next_field!(args, title)?;
+    let description = get_next_field!(args, description)?;
+
+    let payload = AddCategoryPayload { title, description };
+
+    run_client_action(|client| client.add_category(payload));
+    Ok(())
+}
+
+fn run_edit_category<'a>(mut args: impl Iterator<Item = &'a str>) -> Fallible<()> {
+    let id = get_next_id!(args, u32 => category_id)?;
+    let title = get_next_field!(args, title).ok();
+    let description = get_next_field!(args, description).ok();
+
+    let payload = EditCategoryPayload {
+        id,
+        title,
+        description,
+    };
+
+    run_client_action(|client| client.edit_category(payload));
+    Ok(())
+}
+
+fn run_hide_category<'a>(mut args: impl Iterator<Item = &'a str>) -> Fallible<()> {
+    let id = get_next_id!(args, u32 => category_id)?;
+
+    let payload = HideCategoryPayload { id, hide: true };
+
+    run_client_action(|client| client.hide_category(payload));
+    Ok(())
+}
 
 service! {
     rpc get_user(payload: GetUserPayload) -> UserPayload | ContentError;
@@ -53,48 +248,25 @@ service! {
     rpc search(payload: SearchPayload) -> SearchResultsPayload | ContentError;
 }
 
-pub enum Cmd {
-    Get,
-    Insert,
+// Connect to server
+fn connect() -> Option<SyncClient> {
+    let options = client::Options::default();
+    let addr = "localhost:10000".first_socket_addr();
+
+    SyncClient::connect(addr, options).ok()
 }
 
-impl TryFrom<&str> for Cmd {
-    type Error = ();
-    fn try_from(s: &str) -> Result<Self, Self::Error> {
-        use self::Cmd::*;
-        match s {
-            "get" => Ok(Get),
-            "insert" => Ok(Insert),
-            _ => Err(()),
-        }
-    }
-}
-
-pub enum Mode {
-    Main,
-    Users,
-    Categories,
-}
-
-impl TryFrom<&str> for Mode {
-    type Error = ();
-    fn try_from(s: &str) -> Result<Self, Self::Error> {
-        use self::Mode::*;
-        match s {
-            "main" => Ok(Main),
-            "users" => Ok(Users),
-            "categories" => Ok(Categories),
-            _ => Err(()),
-        }
-    }
-}
-
-impl Mode {
-    pub fn as_str(&self) -> &'static str {
-        match self {
-            Mode::Main => "main",
-            Mode::Users => "users",
-            Mode::Categories => "categories",
+// Run a action on the server and print the result
+fn run_client_action<T, E, F>(f: F)
+where
+    T: Debug,
+    E: Debug,
+    F: FnOnce(SyncClient) -> Result<T, E>,
+{
+    if let Some(client) = connect() {
+        match f(client) {
+            Ok(value) => println!("The server responded with: {:#?}", value),
+            Err(error) => println!("The server responded with error: {:#?}", error),
         }
     }
 }
@@ -126,7 +298,7 @@ fn main() {
         println!("No previous history.");
     }
     loop {
-        let readline = rl.readline(&format!("{} >> ", state.mode.as_str()));
+        let readline = rl.readline(&format!("{} >> ", state.mode));
         match readline {
             Ok(line) => {
                 rl.add_history_entry(line.as_ref());
@@ -136,117 +308,15 @@ fn main() {
                         .nth(1)
                         .map(|mode_str| state.try_set_mode(mode_str));
                 } else {
-                    cmd_handler(&state, &line).unwrap_or_else(|err| println!("Error: {:?}", err));
+                    cmd_handler(&state, &line).unwrap_or_else(|err| println!("Error: {}", err));
                 }
             }
             Err(ReadlineError::Interrupted) | Err(ReadlineError::Eof) => break,
             Err(err) => {
-                println!("Error: {:?}", err);
+                println!("Error: {}", err);
                 break;
             }
         }
     }
     rl.save_history("history.txt").unwrap();
-}
-
-fn cmd_handler<'a>(state: &State, s: &'a str) -> Result<(), std::option::NoneError> {
-    let mut words = s.split(char::is_whitespace);
-    let cmd = words.next().and_then(|w| w.try_into().ok())?;
-    match state.mode {
-        Mode::Users => match cmd {
-            Cmd::Get => {
-                let id = words.next().and_then(|w| w.parse().ok())?;
-                run_get_user(id);
-                Ok(())
-            }
-            Cmd::Insert => {
-                let id = words.next().and_then(|w| w.parse().ok())?;
-                let username = words
-                    .next()
-                    .and_then(|w| Username::try_from(w.to_string()).ok())?;
-                run_add_user(id, username);
-                Ok(())
-            }
-        },
-        Mode::Categories => match cmd {
-            Cmd::Get => {
-                let id = words.next().and_then(|w| w.parse().ok())?;
-                run_get_category(id);
-                Ok(())
-            }
-            Cmd::Insert => {
-                run_add_category();
-                Ok(())
-            }
-        },
-        _ => Ok(()),
-    }
-}
-
-// Users
-
-fn run_get_user(id: u32) {
-    // Build request
-    let request = GetUserPayload { id: id.into() };
-
-    // Call
-    run_client_action(move |client| client.get_user(request));
-}
-
-fn run_add_user(id: u32, username: Username) {
-    // Build request
-    let request = AddUserPayload {
-        id: id.into(),
-        username,
-    };
-
-    // Call
-    run_client_action(|client| client.add_user(request));
-}
-
-// Categories
-
-fn run_get_category(id: u32) {
-    let request = GetCategoryPayload {
-        id: id.into(),
-        include_hidden: true,
-    };
-
-    run_client_action(|client| client.get_category(request));
-}
-
-fn run_add_category() {
-    // Build request
-    let title = Title::try_from("Test cat title".to_string()).expect("Invalid title");
-    let description =
-        Description::try_from("Test cat description".to_string()).expect("Invalid description");
-    let request = AddCategoryPayload { title, description };
-
-    // Call
-    run_client_action(|client| client.add_category(request));
-}
-
-// Connect to server
-fn connect() -> Option<SyncClient> {
-    let options = client::Options::default();
-    let addr = "localhost:10000".first_socket_addr();
-
-    SyncClient::connect(addr, options)
-        .map_err(|e| println!("Error connecting: {:#?}", e))
-        .ok()
-}
-
-// Run a action on the server and print the result
-fn run_client_action<T, E, F>(f: F)
-where
-    T: Debug,
-    E: Debug,
-    F: FnOnce(SyncClient) -> Result<T, E>,
-{
-    if let Some(client) = connect() {
-        match f(client) {
-            Ok(value) => println!("The server responded with: {:#?}", value),
-            Err(error) => println!("The server responded with error: {:#?}", error),
-        }
-    }
 }
